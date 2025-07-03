940b1c961807b03e2da5f9895c770325963d651d
Naveen Kumar J <naveen.jayakumar02@gmail.com>
2025-07-03 23:36:45 +0530

    Made minor Port Change

--- backend/server.js
+++ backend/server.js
@@ -9,7 +9,7 @@ const orderRouter = require("./routes/orderRoute.js");
 dotenv.config();
 
 const app = express();
-const PORT = process.env.PORT || 4000;
+const PORT = process.env.PORT || 8080;
 
 app.use(express.json());
 app.use(cors());
